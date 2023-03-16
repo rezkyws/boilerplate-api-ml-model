c7629f66b0ba72459ac3205df9ea61f4dc6fb2ce
Rezky Wahyuda Sitepu <rezkywahyudasitepu@gmail.com>
2023-03-16 11:59:32 +0700

    add service template

--- app/api/api_v1/services/service_1.py
+++ app/api/api_v1/services/service_1.py
@@ -1,1 +1,20 @@
 # module specific business logic (will be use for endpoints)
+from ..load_models import models
+from ....core.logging import logger
+
+
+class ServiceOne:
+    def __init__(self) -> None:
+        pass
+
+    def get_inference(self, text):
+        try:
+            # do something here
+            text = models.the_model
+
+            return text, 1, None
+        
+        except Exception as e:
+            logger.error('error while getting something :', e, 'with input :', text)
+
+            return '', 0, e
