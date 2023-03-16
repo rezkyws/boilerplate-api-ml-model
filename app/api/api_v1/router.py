e3d00324cb85bdcd10ac531ed733f4f6f21acc68
Rezky Wahyuda Sitepu <rezkywahyudasitepu@gmail.com>
2023-03-16 10:37:11 +0700

    add template router

--- app/api/api_v1/router.py
+++ app/api/api_v1/router.py
@@ -1,1 +1,18 @@
 # api core module for all endpoints
+from fastapi import APIRouter
+from .endpoints.endpoint_name import ClassName
+from .schemas.doc import Doc
+
+router = APIRouter(
+    prefix='/api/v1',
+    responses = {
+        404: {'description': 'Not Found'}
+    }
+)
+
+@router.post('/endpoint-name')
+async def func_name(doc: Doc):
+    class_name = ClassName(doc.text)
+    result = class_name.get_inference()
+
+    return result
